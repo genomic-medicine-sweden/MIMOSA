e9b95a243ece4d7a3346b36a3ea680d34fdba1d1
Andersson Olivia <olivia.andersson@regionorebrolan.se>
2025-11-19 16:36:53 +0100

    Clean up formatting

--- frontend/src/utils/distance.js
+++ frontend/src/utils/distance.js
@@ -16,4 +16,3 @@ const calculateDistance = (lat1, lon1, lat2, lon2) => {
 };
 
 export default calculateDistance;
-
--- frontend/src/utils/info.js
+++ frontend/src/utils/info.js
@@ -13,4 +13,3 @@ export function generateInfoContent(countyName, countyData) {
     }
   `;
 }
-
