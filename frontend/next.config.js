fba69549520cf88b283b506367f4c5008a36dfe9
Andersson Olivia <olivia.andersson@regionorebrolan.se>
2026-04-07 10:10:26 +0200

    add @shared webpack alias in Next.js config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,15 @@
 const path = require("path");
 const dotenv = require("dotenv");
-
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
 const domain = process.env.DOMAIN || "localhost";
 
 const nextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: [`${domain}`],
+  webpack: (config) => {
+    config.resolve.alias["@shared"] = path.resolve(__dirname, "../shared");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
